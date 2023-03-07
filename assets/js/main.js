38a694e7c3209017c095ec82fb0cfd1e9611dd1d
Richard5P <richard.deutsch@gmail.com>
2023-03-07 17:21:20 +0000

    Feat: Add js window.onload function to insert html template literal

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Used to immediately check the orientation on loading
+ */
+window.onload = function () {
+    let html = `<h2>Welcome to the Commie Quiz!</h2>
+    <p>A fun game where you can test your current knowledge about Communism and find resources for expanding on
+        it.</p>`;
+    document.getElementById("home-area").innerHTML = html;
+  };
+
