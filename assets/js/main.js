d0a91c2fba0de19a195cc0af74d195c5b080c9af
Richard5P <richard.deutsch@gmail.com>
2023-03-09 16:21:00 +0000

    dev: Add event listener to turn on/off visibility by className

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,21 +15,48 @@ window.onload = function () {
         }
     }
   */
-  
+
 
 window.onload = function () {
-    document.getElementById("welcome").style.visibility = "visible";
+    document.getElementById("welcome-panel").style.visibility = "visible";
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     console.log("in listener");
-    document.getElementById("start-game").addEventListener("click", function(){
-        console.log("clicked");
-        if (document.getElementById("game-area").style.backgroundColor == "magenta") {
-             document.getElementById("game-area").style.backgroundColor = "aqua";
-        } else {
-            document.getElementById("game-area").style.backgroundColor = "magenta";
-        }
-        })
-        })
-    
+    document.getElementById("start-game").addEventListener("click", bcChange);
+
+    document.getElementById("btn-menu").addEventListener("click", displayPanel);
+
+    displayChildren();
+
+})
+
+
+function bcChange() {
+    if (document.getElementById("welcome-panel").style.backgroundColor == "magenta") {
+        document.getElementById("welcome-panel").style.backgroundColor = "aqua";
+    } else {
+        document.getElementById("welcome-panel").style.backgroundColor = "magenta";
+    }
+}
+
+
+function displayPanel() {
+    console.log("in displayPanel");
+    let classElmts = document.getElementsByClassName("welcome");
+    for (let i = 0; i < classElmts.length; i++) {
+        classElmts[i].style.visibility = "hidden";
+    }
+    let classElmts2 = document.getElementsByClassName("menu");
+    for (let i = 0; i < classElmts2.length; i++) {
+        classElmts2[i].style.visibility = "visible";
+    }
+}
+
+
+function displayChildren() {
+    let divKids = document.getElementById("game-area");
+    for (let i = 0; i < divKids.length; i++) {
+        console.log(divKids[i].tagName);
+    }
+}
