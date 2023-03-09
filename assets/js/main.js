5b48e3319a04024c3cae4046fd7cdf77f6448c9a
Richard5P <richard.deutsch@gmail.com>
2023-03-09 10:36:44 +0000

    dev: Add button and event listener (as tests)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,35 @@
 /**
- * Used to immediately check the orientation on loading
+ * Load
  */
+
+/*
 window.onload = function () {
-    let html = `<h2>Welcome to the Commie Quiz!</h2>
-    <p>A fun game where you can test your current knowledge about Communism and find resources for expanding on
-        it.</p>`;
-    document.getElementById("home-area").innerHTML = html;
-  };
+    document.getElementById("welcome").style.visibility = "visible";
+    console.log("before listener");
+    document.addEventListener("DOMContentLoaded", function(){
+        console.log("in listener");
+        document.getElementById("start-game").addEventListener("click"), function(){
+            console.log("clicked");
+            document.getElementById("game-area").style.backgroundColor = "magenta";
+            }
+        }
+    }
+  */
   
 
+window.onload = function () {
+    document.getElementById("welcome").style.visibility = "visible";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("in listener");
+    document.getElementById("start-game").addEventListener("click", function(){
+        console.log("clicked");
+        if (document.getElementById("game-area").style.backgroundColor == "magenta") {
+             document.getElementById("game-area").style.backgroundColor = "aqua";
+        } else {
+            document.getElementById("game-area").style.backgroundColor = "magenta";
+        }
+        })
+        })
+    
