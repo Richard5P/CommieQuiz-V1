0ae94b8ecdc6628515f24d7c64e1c8a735f63aa8
Richard5P <richard.deutsch@gmail.com>
2023-03-09 19:13:00 +0000

    dev: Add more event listeners, displayQuestion function featuring loops and template literal as examples of potential coding approaches - note: much of this code was for experimentation and for upcoming mentor session, it will be replaced with more project specific code

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,8 @@ window.onload = function () {
     }
   */
 
+/* Replace individual listener assignment with class loop and have function with case to action */
+
 
 window.onload = function () {
     document.getElementById("welcome-panel").style.visibility = "visible";
@@ -43,13 +45,30 @@ function bcChange() {
 
 function displayPanel() {
     console.log("in displayPanel");
-    let classElmts = document.getElementsByClassName("welcome");
-    for (let i = 0; i < classElmts.length; i++) {
-        classElmts[i].style.visibility = "hidden";
+
+    if (document.getElementById("welcome-panel").style.visibility === 'hidden') {
+        document.getElementById("welcome-panel").style.visibility = 'visible';
+        document.getElementById("menu-panel").style.visibility = 'hidden';
+    } else {
+        document.getElementById("welcome-panel").style.visibility = 'hidden';
+        document.getElementById("menu-panel").style.visibility = 'visible';
     }
-    let classElmts2 = document.getElementsByClassName("menu");
-    for (let i = 0; i < classElmts2.length; i++) {
-        classElmts2[i].style.visibility = "visible";
+}
+
+function displayQuestion () {
+    let quizArea = document.getElementById("quiz-panel");
+    let resultsArea = document.getElementById("results-area");
+    for (question of quizQuestions){
+        let correctAnswer = question.answer;
+        let quizHTML = `<h2>${question.question}</h2>
+        <button id="btn-answer-1" class="btn-answer">${question.a}</button>
+        <button id="btn-answer-1" class="btn-answer">${question.b}</button>
+        <button id="btn-answer-1" class="btn-answer">${question.c}</button>
+        <button id="btn-answer-1" class="btn-answer">${question.d}</button>`;
+        quizArea.innerHTML=quizHTML;
+        if(!runQuiz(question.answer)){
+            /* append answer and linkto to resultsPanel */
+        }
     }
 }
 
