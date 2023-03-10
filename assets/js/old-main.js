4d1008292c13270d5b26b59d2f9a769655c4443c
Richard5P <richard.deutsch@gmail.com>
2023-03-10 10:54:43 +0000

    dev: Add button style, menu buttons style and listener, rules panel

---
+++ assets/js/old-main.js
@@ -0,0 +1,88 @@
+/**
+ * Load
+ */
+
+/*
+window.onload = function () {
+    document.getElementById("welcome").style.visibility = "visible";
+    console.log("before listener");
+    document.addEventListener("DOMContentLoaded", function(){
+        console.log("in listener");
+        document.getElementById("start-game").addEventListener("click"), function(){
+            console.log("clicked");
+            document.getElementById("game-area").style.backgroundColor = "magenta";
+            }
+        }
+    }
+  */
+
+/* Replace individual listener assignment with class loop and have function with case to action */
+
+
+window.onload = function () {
+    document.getElementById("welcome-panel").style.visibility = "visible";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("in listener");
+    document.getElementById("start-game").addEventListener("click", bcChange);
+
+    document.getElementById("menu-button").addEventListener("click", displayPanel);
+
+    document.getElementById("btn-rules").addEventListener("click", displayRules);
+
+    displayChildren();
+
+})
+
+
+function bcChange() {
+    if (document.getElementById("welcome-panel").style.backgroundColor == "magenta") {
+        document.getElementById("welcome-panel").style.backgroundColor = "aqua";
+    } else {
+        document.getElementById("welcome-panel").style.backgroundColor = "magenta";
+    }
+}
+
+
+function displayPanel() {
+    console.log("in displayPanel");
+
+    if (document.getElementById("welcome-panel").style.visibility === 'hidden') {
+        document.getElementById("welcome-panel").style.visibility = 'visible';
+        document.getElementById("menu-panel").style.visibility = 'hidden';
+    } else {
+        document.getElementById("welcome-panel").style.visibility = 'hidden';
+        document.getElementById("menu-panel").style.visibility = 'visible';
+    }
+}
+
+function displayRules(){
+    document.getElementById("menu-panel").style.visibility = 'hidden';
+    document.getElementById("rules-panel").style.visibility = 'visible';
+}
+
+function displayQuestion () {
+    let quizArea = document.getElementById("quiz-panel");
+    let resultsArea = document.getElementById("results-area");
+    for (question of quizQuestions){
+        let correctAnswer = question.answer;
+        let quizHTML = `<h2>${question.question}</h2>
+        <button id="btn-answer-1" class="btn-answer">${question.a}</button>
+        <button id="btn-answer-2" class="btn-answer">${question.b}</button>
+        <button id="btn-answer-3" class="btn-answer">${question.c}</button>
+        <button id="btn-answer-4" class="btn-answer">${question.d}</button>`;
+        quizArea.innerHTML=quizHTML;
+        if(!runQuiz(question.answer)){
+            /* append answer and linkto to resultsPanel */
+        }
+    }
+}
+
+
+function displayChildren() {
+    let divKids = document.getElementById("game-area");
+    for (let i = 0; i < divKids.length; i++) {
+        console.log(divKids[i].tagName);
+    }
+}
