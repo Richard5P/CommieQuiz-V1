2cae543414d296906482b90307c92bbebfaeed3b
Richard5P <richard.deutsch@gmail.com>
2023-03-16 08:21:25 +0000

    style: Update controls-are height

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // Variables
 const homePanelButton = document.getElementById("btn-home");
 const menuPanelButton = document.getElementById("btn-menu");
+const gameTimerDisplay = document.getElementById("game-timer");
+const questionTimerDisplay = document.getElementById("question-timer");
 const correctAnswerDisplay = document.getElementById("correct-answers");
 const panelArea = document.getElementById("panel-area");
 const welcomePanel = document.getElementById("welcome-panel");
@@ -8,16 +10,24 @@ const menuPanel = document.getElementById("menu-panel");
 const rulesPanel = document.getElementById("rules-panel");
 const quizPanel = document.getElementById("quiz-panel");
 const scoresPanel = document.getElementById("scores-panel");
+const welcomeButton = document.getElementById("btn-welcome");
+const quizButton = document.getElementById("btn-quiz");
+const rulesButton = document.getElementById("btn-rules");
+const scoresButton = document.getElementById("btn-scores");
 const quizQuestion = document.getElementById("quiz-question");
 const quizAnswer1 = document.getElementById("btn-answer-1");
 const quizAnswer2 = document.getElementById("btn-answer-2");
 const quizAnswer3 = document.getElementById("btn-answer-3");
 const quizAnswer4 = document.getElementById("btn-answer-4");
+const nextQuestion = document.getElementById("btn-next");
+const gameStartingMinutes = 2;
+let gameTime = gameStartingMinutes * 60;
+let questionTime = 5; // this should change for the user option
 
 let currentPanel = "welcome-panel";
 let quizCorrectAnswer = "";
 let correctAnswerCount = 0;
-let quizTimerBool = true;
+let gameTimerBool = true;
 let questionTimerBool = true;
 let nextQuestionBool = true;
 
@@ -39,59 +49,66 @@ document.addEventListener("DOMContentLoaded", function () {
     menuPanelButton.addEventListener("click", stopCountDown);
 
     // menu panel
-    document.getElementById("btn-welcome").addEventListener("click", displayHomePanel);
-    document.getElementById("btn-quiz").addEventListener("click", displayQuizPanel);
-    document.getElementById("btn-rules").addEventListener("click", displayRulesPanel);
-    document.getElementById("btn-scores").addEventListener("click", displayScoresPanel);
+    welcomeButton.addEventListener("click", displayHomePanel);
+    quizButton.addEventListener("click", displayQuizPanel);
+    rulesButton.addEventListener("click", displayRulesPanel);
+    scoresButton.addEventListener("click", displayScoresPanel);
 
     //quiz panel
-    document.getElementById("btn-answer-1").addEventListener("click", function () {
+    quizAnswer1.addEventListener("click", function () {
         checkAnswer("A");
     });
-    document.getElementById("btn-answer-2").addEventListener("click", function () {
+    quizAnswer2.addEventListener("click", function () {
         checkAnswer("B");
     });
-    document.getElementById("btn-answer-3").addEventListener("click", function () {
+    quizAnswer3.addEventListener("click", function () {
         checkAnswer("C");
     });
-    document.getElementById("btn-answer-4").addEventListener("click", function () {
+    quizAnswer4.addEventListener("click", function () {
         checkAnswer("D");
     });
-    document.getElementById("btn-answer-1").addEventListener("keydown", function () {
+    quizAnswer1.addEventListener("keydown", function () {
         checkAnswer("A");
     });
-    document.getElementById("btn-answer-2").addEventListener("keydown", function () {
+    quizAnswer2.addEventListener("keydown", function () {
         checkAnswer("B");
     });
-    document.getElementById("btn-answer-3").addEventListener("keydown", function () {
+    quizAnswer3.addEventListener("keydown", function () {
         checkAnswer("C");
     });
-    document.getElementById("btn-answer-4").addEventListener("keydown", function () {
+    quizAnswer4.addEventListener("keydown", function () {
         checkAnswer("D");
     });
-    document.getElementById("btn-next").addEventListener("click", nextQuestion);
-    document.getElementById("btn-next").addEventListener("click", stopCountDown);
-    document.getElementById("btn-next").addEventListener("keydown", nextQuestion);
-    document.getElementById("btn-next").addEventListener("keydown", stopCountDown);
+    nextQuestion.addEventListener("click", function(){
+        console.log("Next Question");
+    });
+    nextQuestion.addEventListener("click", loadNextQuestion);
+    nextQuestion.addEventListener("click", stopCountDown);
+    nextQuestion.addEventListener("keydown", loadNextQuestion);
+    nextQuestion.addEventListener("keydown", stopCountDown);
 });
 
-function hidePanels(){
+function hidePanels() {
+// hide timers as well
+    gameTimerDisplay.classList.add("hidden");
+    questionTimerDisplay.classList.add("hidden");    
+
     let panels = document.getElementsByClassName("panel");
-    for (let i=0;i < panels.length; i++)
+    for (let i = 0; i < panels.length; i++)
         panels[i].classList.add("hidden");
 }
 
 function displayHomePanel() {
     console.log("Home Panel");
     hidePanels();
-    welcomePanel.classList.remove ("hidden");
+    welcomePanel.classList.remove("hidden");
     currentPanel = "welcome-panel";
 }
 
 function displayMenuPanel() {
     console.log("Menu Panel");
     hidePanels();
-    menuPanel.classList.remove ("hidden");
+    menuPanel.classList.remove("hidden");
     currentPanel = "menu-panel";
 }
 
@@ -99,22 +116,22 @@ function displayMenuPanel() {
 function displayQuizPanel() {
     console.log("Quiz Panel");
     hidePanels();
-    quizPanel.classList.remove ("hidden");
+    quizPanel.classList.remove("hidden");
     currentPanel = "quiz-panel";
     loadQuizQuestions();
-    }
+}
 
 function displayRulesPanel() {
     console.log("Rules Panel");
     hidePanels();
-    rulesPanel.classList.remove ("hidden");
+    rulesPanel.classList.remove("hidden");
     currentPanel = "rules-panel";
 }
 
 function displayScoresPanel() {
     console.log("Scores Panel");
     hidePanels();
-    scoresPanel.classList.remove ("hidden");
+    scoresPanel.classList.remove("hidden");
     currentPanel = "scores-panel";
 }
 
@@ -122,53 +139,75 @@ function loadQuizQuestions() {
     console.log("load Quiz Questions");
     document.getElementById("correct-answers").value = 0;
     for (let currentQuestion of quizQuestions) {
-        do {
-        quizQuestion.innerText = currentQuestion.question;
-        quizAnswer1.innerText = currentQuestion.a;
-        quizAnswer2.innerText = currentQuestion.b;
-        quizAnswer3.innerText = currentQuestion.c;
-        quizAnswer4.innerText = currentQuestion.d;
-        quizCorrectAnswer = currentQuestion.answer;
-        console.log(quizCorrectAnswer);
-        questionTimerBool = true;
-        }
-        while(displayQuestionCountDown() && questionTimerBool);
-        if (nextQuestionBool){
-            continue;
-        }
-        else {
-            break;
-        }
-    }
+            quizQuestion.innerText = currentQuestion.question;
+            quizAnswer1.innerText = currentQuestion.a;
+            quizAnswer2.innerText = currentQuestion.b;
+            quizAnswer3.innerText = currentQuestion.c;
+            quizAnswer4.innerText = currentQuestion.d;
+            quizCorrectAnswer = currentQuestion.answer;
+            console.log(quizCorrectAnswer);
+            questionTimerBool = true;
+            console.log("in for before wait");
+            waitResponse();
+            console.log("in for after wait");
+        };
 }
+
+/*        if (nextQuestionBool) {
+            continue;
+        } else {
+            break;
+        }*/
 
 
 function checkAnswer(selectedAnswer) {
     correctAnswerDisplay.innerText = quizCorrectAnswer === selectedAnswer ? ++correctAnswerCount : correctAnswerCount;
 }
 
-// Add displayGameCountDown 
+// Timers
+//setInterval(gameCountDown, 1000);
 
-function displayQuestionCountDown() {
-    // Set the date we're counting down to
-    var countDownDate = new Date(Date.now() + (1000 * 60 * 2) + 2);
+function gameCountDown() {
+    const minutes = Math.floor(gameTime / 60);
+    do {
+        let seconds = gameTime % 60;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        gameTimerDisplay.innerHTML = `${minutes}:${seconds}`;
+    }
+    while (gameTime-- > 0 && gameTimerBool);
+}
 
-    // Update the count down every 1 second
-    var x = setInterval(function () {
-        // Get today's date and time
-        var now = new Date().getTime();
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-        // Calculate the minutes and seconds
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("timer").innerHTML = `${minutes} : ${seconds}`;
-        //If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            return(false);
-        }
-    }, 1000);
+function waitResponse () {
+    const questionInterval = setInterval(questionCountDown, 1000);
+    let time = 5;
+    questionTimerDisplay.classList.remove("hidden");
+    function questionCountDown() {
+        console.log("QuestionCountDown Time", time);
+        let remainingSeconds = time % 60;
+        remainingSeconds = remainingSeconds < 10 ? '0'+ remainingSeconds : remainingSeconds;
+        questionTimerDisplay.innerHTML = `${remainingSeconds}`;
+
+       if (time >= 1){
+          time--;
+        } else {
+          clearInterval(questionInterval);
+          nextQuestionBool = true;
+          return(true);
+        }; 
+    }
+}
+
+
+
+
+function MyquestionCountDown() {
+    questionTimerDisplay.classList.remove("hidden");
+    do {
+        let seconds = questionTime;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        questionTimerDisplay.innerHTML = `${seconds}`;
+    }
+    while (questionTime-- > 0 && questionTimerBool);
 }
 
 function stopCountDown() {
@@ -176,6 +215,6 @@ function stopCountDown() {
     nextQuestionBool = false;
 }
 
-function nextQuestion(){
+function loadNextQuestion() {
     nextQuestionBool = true;
 }
